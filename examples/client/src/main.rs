c0eaba6d65dba2b8dca30effbfeb6be23ee2e152
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2024-11-26 17:17:21 +0100

    Add ioctl resolver to get the token in client example
    Signed-off-by: Lukasz Pawelczyk <l.pawelczyk@samsung.com>

--- examples/client/src/main.rs
+++ examples/client/src/main.rs
@@ -1,9 +1,11 @@
-use std::{sync::Arc};
+mod resolver;
+
 use std::io::Write;
+use std::sync::Arc;
 
 use clap::Parser;
 use log::info;
-use ratls::{RaTlsClient, TokenFromFile};
+use ratls::{InternalTokenResolver, RaTlsClient, TokenFromFile};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
@@ -12,17 +14,17 @@ struct Args {
     #[arg(short, long)]
     root_ca: String,
 
-    /// Url to ratls server
+    /// URL to ratls server
     #[arg(short = 'u', long, default_value = "localhost:1337")]
     server_url: String,
 
-    /// Server name, overriden if server is attested
+    /// Server name, overridden if server is attested
     #[clap(short = 'n', long, default_value = "localhost")]
     server_name: String,
 
-    /// Use dummy token from file (usefull for testing)
+    /// Use dummy token from file (useful for testing)
     #[arg(short, long)]
-    token: Option<String>
+    token: Option<String>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -30,9 +32,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args = Args::parse();
 
+    let resolver: Arc<dyn InternalTokenResolver> = if let Some(token) = args.token {
+        Arc::new(TokenFromFile::from_path(token)?)
+    } else {
+        Arc::new(resolver::IoctlTokenResolver())
+    };
+
     let client = RaTlsClient::new(ratls::ClientMode::AttestedClient {
-        client_token_resolver: Arc::new(TokenFromFile::from_path(args.token.expect("In demo mode token needs to be provided from file"))?),
-        root_ca_path: args.root_ca
+        client_token_resolver: resolver,
+        root_ca_path: args.root_ca,
     })?;
 
     let mut connection = client.connect(args.server_url, args.server_name)?;
